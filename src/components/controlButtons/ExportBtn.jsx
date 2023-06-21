f94d03ab67640d5f54e12cc0ee0a7d1b6f7c232d
SidimaD <43736678+Sidimadtv@users.noreply.github.com>
2023-06-21 15:36:59 +0200

    las

--- src/components/controlButtons/ExportBtn.jsx
+++ src/components/controlButtons/ExportBtn.jsx
@@ -5,8 +5,8 @@ const ExportBtn = () => {
 
   return (
     // Exporting all passwords to an encrypted JSON file when clicked
-    <button onClick={() => {clickedForSetter('ExportToFile') && showPasswordPrompt()}} className="text-white font-bold hover:bg-blue-700 bg-red-500 rounded px-4 py-2 my-2 sm:my-0">
-      <i class="fa fa-database" style="font-size:36px;color:#ffffff"></i>-Back Up Now
+    <button  onClick={() => {clickedForSetter('ExportToFile') && showPasswordPrompt()}} className="text-white font-bold hover:bg-blue-700 bg-red-500 rounded px-4 py-2 my-2 sm:my-0">
+      |-Back Up Now
     </button>
   )
 }
--- src/components/controlButtons/ImportBtn.jsx
+++ src/components/controlButtons/ImportBtn.jsx
@@ -6,7 +6,7 @@ const ImportBtn = () => {
   return (
     // Importing passwords from an encrypted JSON file
     <button onClick={() => {clickedForSetter('ImportFromFile') && showPasswordPrompt()}} className="text-white font-bold hover:bg-blue-700 bg-red-500 rounded px-4 py-2 my-2 sm:my-0">
-       <i class="fa fa-web" style="font-size:36px;color:#ffffff"></i>Restore a BackUp
+       |-Restore a BackUp
     </button>
   )
 }
