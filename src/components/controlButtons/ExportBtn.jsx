87ff50cd1eaac7a8adea2d827271680380ec0759
SidimaD <43736678+Sidimadtv@users.noreply.github.com>
2023-06-21 08:03:23 +0200

    upd21

--- src/components/controlButtons/ExportBtn.jsx
+++ src/components/controlButtons/ExportBtn.jsx
@@ -5,7 +5,7 @@ const ExportBtn = () => {
 
   return (
     // Exporting all passwords to an encrypted JSON file when clicked
-    <button onClick={() => {clickedForSetter('ExportToFile') && showPasswordPrompt()}} className="text-black font-bold hover:bg-yellow-600 bg-yellow-500 rounded px-2 py-1 my-2 sm:my-0">
+    <button onClick={() => {clickedForSetter('ExportToFile') && showPasswordPrompt()}} className="text-black font-bold hover:bg-yellow-600 bg-red-500 rounded px-2 py-1 my-2 sm:my-0">
       Back Up Now
     </button>
   )
